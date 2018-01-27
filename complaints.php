78f13cfb74c026ac8bd78592db5e1badcea96e68
dhairyaj <dhairyaj@users.noreply.github.com>
2018-01-27 15:35:07 +0530

    adopt child page

---
+++ complaints.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      
+    <!--Google Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Martel" rel="stylesheet">
+      
+      
+    <style>
+    
+        .dropdown:hover .dropdown-menu {
+          display: block;
+        }
+        .navbar-brand {
+            font-size: 180%;
+            font-weight: bold;
+            font-family: 'Martel', serif;
+        }
+        .nav-link {
+            font-weight: bold;
+        }
+      
+        .navbar-custom {
+          background-color: #d1e0e0;
+
+        }
+
+    </style>
+
+    <title>STARTER TEMPLATE</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+      <a class="navbar-brand" href="#" style="color: #5BE59E;">बालुत प्रिद्र्णं</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              THE ISSUE
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">ARTICLES</a>
+              <a class="dropdown-item" href="#">UPLOAD</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CHAT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">REPORT</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              GET INVOLVED
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">ADOPT</a>
+              <a class="dropdown-item" href="#">DONATE</a>
+              <a class="dropdown-item" href="#">ANALYZE</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+
+    <div>
+      <h1 style="text-align: center;">COMPLAINTS</h1>
+    </div>
+    <div style="padding-left: 10%">
+      <input type="text" placeholder="Name" style="width: 350px;"><br><br>
+      <textarea placeholder="Details" style="width: 350px;height: 150px;"></textarea><br><br>
+      <input type="file">
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      
+    <!--Google Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Martel" rel="stylesheet">
+      
+      
+    <style>
+    
+        .dropdown:hover .dropdown-menu {
+          display: block;
+        }
+        .navbar-brand {
+            font-size: 180%;
+            font-weight: bold;
+            font-family: 'Martel', serif;
+        }
+        .nav-link {
+            font-weight: bold;
+        }
+        .inputBox {
+            border: 2px solid #5BE59E;
+            padding: 5px 10px;
+            border-radius: 10px;
+        }
+        #submit {
+            background: none;
+            border: 2px solid #5BE59E;
+            padding: 5px 10px;
+            color: #5BE59E;
+            font-weight: bold;
+            margin-bottom: 20px;
+            border-radius: 10px;
+        }
+        #submit:hover {
+            background: #5BE59E;
+            color: white;
+        }
+      
+    </style>
+
+    <title>KINDER SALVATION</title>
+  </head>
+  <body style="background: url('images/bg.jpg'); background-size: cover;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 20px;">
+      <a class="navbar-brand" href="#" style="color: #5BE59E;">बालुत प्रिद्र्णं</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="email" placeholder="Email" aria-label="Email">
+          <input class="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">LOGIN</button>
+        </form>
+      </div>
+    </nav>
+      
+    <div id="signup" style="text-align: center; background: white; opacity: 0.8; width: 30%; margin: 0 auto; border-radius: 20px;">
+        <br>
+        <p><input type="text" placeholder="Name" class="inputBox"></p>
+        <p><input type="email" placeholder="Email" class="inputBox"></p>
+        <p><input type="password" placeholder="Password" class="inputBox"></p>
+        <p><input type="password" placeholder="Confirm Password" class="inputBox"></p>
+        <p><input type="number" placeholder="Mobile" class="inputBox"></p>
+        <p><input type="text" placeholder="Address" class="inputBox"></p>
+        <p>I am a: <input type="radio" name="accountType">User&nbsp;&nbsp;<input type="radio" name="accountType">Counselor</p>
+        <p><input type="checkbox">&nbsp;I agree to terms and conditions</p>
+        <p><input type="submit" value="Create Account" id="submit"></p>
+    </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
